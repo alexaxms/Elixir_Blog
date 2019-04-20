2b50861020c48f72b8c72d7c8b998439f9970447
alexaxms <alexaxmss@hotmail.com>
2019-04-20 13:59:33 -0400

    second part

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -1,22 +1,40 @@
 defmodule Pxblog.SessionController do
   use Pxblog.Web, :controller
   alias Pxblog.User
-  import Comeonin.Bcrypt, only: [checkpw: 2]
+  import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
   plug :scrub_params, "user" when action in [:create]
 
   def new(conn, _params) do
     render conn, "new.html", changeset: User.changeset(%User{})
   end
 
-  def create(conn, %{"user" => user_params}) do
-    Repo.get_by(User, username: user_params["username"])
-    |> sign_in(user_params["password"], conn)
+  def create(conn, %{"user" => %{"username" => username, "password" => password}}) when not is_nil(username) and not is_nil(password) do
+    user = Repo.get_by(User, username: username)
+    sign_in(user, password, conn)
   end
 
-  defp sign_in(user, password, conn) when is_nil(user) do
+  def create(conn, _) do
+    failed_login(conn)
+  end
+
+  def delete(conn, _params) do
     conn
+    |> delete_session(:current_user)
+    |> put_flash(:info, "Signed out successfully!")
+    |> redirect(to: page_path(conn, :index))
+  end
+
+  defp failed_login(conn) do
+    dummy_checkpw()
+    conn
+    |> put_session(:current_user, nil)
     |> put_flash(:error, "Invalid username/password combination!")
     |> redirect(to: page_path(conn, :index))
+    |> halt()
+  end
+
+  defp sign_in(user, _password, conn) when is_nil(user) do
+    failed_login(conn)
   end
 
   defp sign_in(user, password, conn) do
@@ -26,17 +44,7 @@ defmodule Pxblog.SessionController do
       |> put_flash(:info, "Sign in successful!")
       |> redirect(to: page_path(conn, :index))
     else
-      conn
-      |> put_session(:current_user, nil)
-      |> put_flash(:error, "Invalid username/password combination!")
-      |> redirect(to: page_path(conn, :index))
+      failed_login(conn)
     end
-  end
-
-  def delete(conn, _params) do
-    conn
-    |> delete_session(:current_user)
-    |> put_flash(:info, "Signed out successfully!")
-    |> redirect(to: page_path(conn, :index))
   end
 end
